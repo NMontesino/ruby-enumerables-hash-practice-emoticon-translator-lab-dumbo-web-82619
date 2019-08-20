43117beee342471b05ff6a84c917e0abaa77de7a
glorious-genre-8033 <namontesino@gmail.com>
2019-08-20 22:49:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ def load_library(path)
   emoticon_names = emoticons.keys
   
   emoticon_names.length.times do |i|
-    get_something[:get_meaning][][i] = 
+    get_something[:get_meaning][emoticons[emoticon_names[i]][1]] 
   end
   
   return get_something
