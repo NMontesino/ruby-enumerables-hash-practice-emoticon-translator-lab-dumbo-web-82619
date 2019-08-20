69e20bb014e4225ef7a3e48682203d449edb10df
glorious-genre-8033 <namontesino@gmail.com>
2019-08-20 22:07:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,8 @@
 # require modules here
 
-def load_library
+def load_library(path)
   require 'yaml'
-  emoticons = YAML.load_file('./lib/emoticons.yml')
+  emoticons = YAML.load_file(path)
 end
 
 def get_japanese_emoticon
