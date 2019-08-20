d35bc4367e17bc0fb0382ce5013c7149dc3f105e
glorious-genre-8033 <namontesino@gmail.com>
2019-08-20 23:39:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -37,7 +37,7 @@ def get_japanese_emoticon(path, emoticon)
   japanese = emoticons[:get_meaning].keys
   
   equivalent = emoticons[:get_emoticon][emoticon]
-  print equivalent
+  return equivalent
   
 end
 
