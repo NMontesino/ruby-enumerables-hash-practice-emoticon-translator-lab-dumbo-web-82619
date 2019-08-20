4a04ad0e7f1e466a4eba8f17b00be2f9aafbd1d7
glorious-genre-8033 <namontesino@gmail.com>
2019-08-20 23:27:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -36,7 +36,7 @@ def get_japanese_emoticon(path, emoticon = nil)
   english = emoticons[:get_emoticon].keys
   japanese = emoticons[:get_meaning].keys
   
-  puts english
+  p english
   
   #emoticons[:get_emoticon][emoticon].find_index
   
